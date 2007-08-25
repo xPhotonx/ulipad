bd5e52b739ac3008fe828c2227b467b33f124f13
limodou <limodou@fb567eef-3936-0410-92ba-89b1ab49dacc>
2007-08-25 13:21:40 +0000

    Add settings menu, and user can set svn path in this menu item.

--- trunk/plugins/svn_plugin/__init__.py
+++ trunk/plugins/svn_plugin/__init__.py
@@ -26,12 +26,12 @@ from modules import Mixin
 import os
 
 def pref_init(pref):
-    pref.version_control_exe = ''
+    pref.svn_exe = ''
 Mixin.setPlugin('preference', 'init', pref_init)
 
 def add_pref(preflist):
     preflist.extend([
-        (tr('Version'), 100, 'text', 'version_control_exe', tr('Select version control software'), None)
+        (tr('Version'), 100, 'text', 'svn_exe', tr('Select location of subversion client'), None)
     ])
 Mixin.setPlugin('preference', 'add_pref', add_pref)
 
@@ -40,39 +40,47 @@ def other_popup_menu(dirwin, projectname, menus):
     
     item = dirwin.tree.GetSelection()
     if not item.IsOk(): return
-    if not detect_svn(common.getCurrentDir(dirwin.get_node_filename(item))):
-        return
+    is_svn_dir = detect_svn(common.getCurrentDir(dirwin.get_node_filename(item)))
     menus.extend([ (None,
         [
-            (93, 'IDPM_VC_UPDATE', tr('Version Control: Update'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            (94, 'IDPM_VC_CHECKOUT', tr('Version Control: Checkout'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            (95, 'IDPM_VC_COMMIT', tr('Version Control: Commit'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            (96, 'IDPM_VC_COMMANDS', tr('Version Control: Commands'), wx.ITEM_NORMAL, '', ''),
-            (97, '', '-', wx.ITEM_SEPARATOR, None, ''),
+            (93.3, 'IDPM_VC_COMMANDS', tr('SVN Commands'), wx.ITEM_NORMAL, '', ''),
+            (93.4, '', '-', wx.ITEM_SEPARATOR, None, ''),
         ]),
         ('IDPM_VC_COMMANDS',
         [
-            (100, 'IDPM_VC_COMMANDS_LIST', tr('List'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            (110, 'IDPM_VC_COMMANDS_SHOWLOG', tr('Show log'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            (120, 'IDPM_VC_COMMANDS_STATUS', tr('Status'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            (130, 'IDPM_VC_COMMANDS_DIFF', tr('Diff'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            (145, '', '-', wx.ITEM_SEPARATOR, None, ''),
-            (150, 'IDPM_VC_COMMANDS_ADD', tr('Add'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            (160, 'IDPM_VC_COMMANDS_RENAME', tr('Rename'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            (170, 'IDPM_VC_COMMANDS_DELETE', tr('Delete'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            (180, 'IDPM_VC_COMMANDS_REVERSE', tr('Revert'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            (190, '', '-', wx.ITEM_SEPARATOR, None, ''),
-            (200, 'IDPM_VC_COMMANDS_EXPORT', tr('Export'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
-            
+            (900, 'IDPM_VC_COMMANDS_SETTINGS', tr('Settings'), wx.ITEM_NORMAL, 'OnVC_Settings', ''),
         ]),
     ])
-#    dir = common.getCurrentDir(dirwin.get_node_filename(item))
-#    if os.path.isdir(dir) and os.path.exists(os.path.join(os.path.dirname(dir), '_project')):
-#        menus.extend([ (None,
-#        [
-#            (520, 'IDPM_DJANGO_INSTALLAPP', tr('Install Application'), wx.ITEM_NORMAL, 'OnDjangoFunc', ''),
-#        ]),
-#    ])
+    
+    if is_svn_dir:
+        menus.extend([ (None,
+            [
+                (93.1, 'IDPM_VC_UPDATE', tr('SVN Update'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+                (93.2, 'IDPM_VC_COMMIT', tr('SVN Commit'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+            ]),
+            ('IDPM_VC_COMMANDS',
+            [
+                (100, 'IDPM_VC_COMMANDS_LIST', tr('List'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+                (110, 'IDPM_VC_COMMANDS_SHOWLOG', tr('Show log'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+                (120, 'IDPM_VC_COMMANDS_STATUS', tr('Status'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+                (130, 'IDPM_VC_COMMANDS_DIFF', tr('Diff'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+                (145, '', '-', wx.ITEM_SEPARATOR, None, ''),
+                (150, 'IDPM_VC_COMMANDS_ADD', tr('Add'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+                (160, 'IDPM_VC_COMMANDS_RENAME', tr('Rename'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+                (170, 'IDPM_VC_COMMANDS_DELETE', tr('Delete'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+                (180, 'IDPM_VC_COMMANDS_REVERSE', tr('Revert'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+                (190, '', '-', wx.ITEM_SEPARATOR, None, ''),
+                (200, 'IDPM_VC_COMMANDS_EXPORT', tr('Export'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+                (210, '', '-', wx.ITEM_SEPARATOR, None, ''),
+                (220, 'IDPM_VC_COMMANDS_SETTINGS', tr('Settings'), wx.ITEM_NORMAL, 'OnVC_Settings', ''),
+            ]),
+        ])
+    else:
+        menus.extend([ (None,
+            [
+                (93, 'IDPM_VC_CHECKOUT', tr('SVN Checkout'), wx.ITEM_NORMAL, 'OnVC_DoCommand', ''),
+            ]),
+        ])
 Mixin.setPlugin('dirbrowser', 'other_popup_menu', other_popup_menu)
 
 def OnVC_DoCommand(win, event):
@@ -101,6 +109,20 @@ def OnVC_DoCommand(win, event):
         if _id == getattr(win, id, None):
             vc.do(win, cmd, path)
 Mixin.setMixin('dirbrowser', 'OnVC_DoCommand', OnVC_DoCommand)
+
+def OnVC_Settings(win, event):
+    dialog = [
+            ('openfile', 'svn_exe', win.pref.svn_exe, tr('Select location of subversion client'), None),
+        ]
+    from modules.EasyGuider import EasyDialog
+    dlg = EasyDialog.EasyDialog(win.mainframe, title=tr("SVN Settings"), elements=dialog)
+    values = None
+    if dlg.ShowModal() == wx.ID_OK:
+        values = dlg.GetValue()
+        win.pref.svn_exe = values['svn_exe']
+        win.pref.save()
+    dlg.Destroy()
+Mixin.setMixin('dirbrowser', 'OnVC_Settings', OnVC_Settings)
 
 #functions
 ########################################################
