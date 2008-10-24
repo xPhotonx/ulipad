a5b8f960d7409e8ba1794eea2a189fd92cbcaaa8
limodou <limodou@fb567eef-3936-0410-92ba-89b1ab49dacc>
2008-10-24 04:29:48 +0000

    apply jquery patch thanks to programmer.py

--- acp/jquery/jquery.py
+++ acp/jquery/jquery.py
@@ -1,10 +1,16 @@
 import os
+import wx.stc
 from modules import dict4ini
 from modules import Globals
 
 _fmtime = None
 _ini = None
 _ini_filename = os.path.join(Globals.workpath, 'acp/jquery/jquery.tag')
+
+# Lookups to determine what the current line-ending is.
+_eols = { wx.stc.STC_EOL_CR : '\r',
+          wx.stc.STC_EOL_CRLF : '\r\n',
+          wx.stc.STC_EOL_LF : '\n' }
 
 def get_newest_ini():
     global _ini
@@ -31,10 +37,61 @@ def calltip(win, word, syncvar):
         line = win.GetCurrentLine()
         linePos=win.PositionFromLine(line)
         if pos > linePos:
-            word = win.getChar(pos - 1)       
+            word = win.getChar(pos - 1)
     return ini.functions.get(word, '')
 
 def autodot(win, word, syncvar):
     ini = get_newest_ini()
-    return ini.default.dots
 
+    # Only autodot when following a $ or a $(...)
+    pos = win.GetCurrentPos()
+    prev_char = win.getChar(pos - 2)
+    if prev_char == '$':
+        return ini.default.dots
+
+    if following_jquery_func(win):
+        return ini.default.dots
+
+    # No hit.
+    return ''
+
+def at_eol(win, pos):
+    'Return True if pos is pointing to the end of a line.'
+    char = win.getChar(pos)
+
+    eol_type = _eols[win.GetEOLMode()]
+    if len(eol_type) == 1:
+        return char == eol_type
+
+    prev_char = win.getChar(pos - 1)
+    return prev_char + char == eol_type
+
+def backwards_match(txt, win, pos):
+    'Returns true if txt is found at win, pos going backwards.'
+    if win.FindText(pos - len(txt), pos, txt, 0) != -1:
+        # Deselect what FindText selects.
+        win.SetSelection(-1,-1)
+        return True
+    return False
+
+def following_jquery_func(win):
+    'Returns true if the cursor is following a jquery function call $(...).'
+    # Try searching for $(...).  Only search up 5 lines.
+    MAX_LINE_SEARCH = 5
+    prev_char_pos = win.GetCurrentPos() - 2
+    if prev_char_pos < 0 or win.getChar(prev_char_pos) != ')':
+        return False
+
+    # OK, start scanning backwards looking for a matching '('
+    lines_searched = 0
+    pos = prev_char_pos
+    while lines_searched < MAX_LINE_SEARCH and prev_char_pos > 0:
+        pos -= 1
+        char = win.getChar(prev_char_pos)
+        if at_eol(win, pos):
+            lines_searched += 1
+            continue
+
+        if backwards_match('$(', win, pos):
+            return True
+    return False
