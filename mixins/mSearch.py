743e9bedbb619105a8c48021818f7a2c2eb9cae0
limodou <limodou@fb567eef-3936-0410-92ba-89b1ab49dacc>
2008-10-24 04:47:28 +0000

    fix typo "Go To Last Modify" to "Go To Last Modified"

--- mixins/mSearch.py
+++ mixins/mSearch.py
@@ -40,7 +40,7 @@ def add_mainframe_menu(menulist):
             (140, 'wx.ID_FORWARD', tr('Find Next') + '\tE=F3', wx.ITEM_NORMAL, 'OnSearchFindNext', tr('Find next occurance of text.')),
             (150, '', '-', wx.ITEM_SEPARATOR, None, ''),
             (160, 'IDM_SEARCH_GOTO_LINE', tr('Go To Line...') + '\tE=Ctrl+G', wx.ITEM_NORMAL, 'OnSearchGotoLine', tr('Goes to specified line in the current document.')),
-            (170, 'IDM_SEARCH_LAST_MODIFY', tr('Go To Last Modify') + '\tE=Ctrl+B', wx.ITEM_NORMAL, 'OnSearchLastModify', tr('Goes to the last modify position.')),
+            (170, 'IDM_SEARCH_LAST_MODIFY', tr('Go To Last Modified') + '\tE=Ctrl+B', wx.ITEM_NORMAL, 'OnSearchLastModify', tr('Goes to the last modify position.')),
 
         ]),
     ])
