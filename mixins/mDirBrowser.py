5ca97c0d123ce91f927ed352e7ff76dfe8963f26
limodou <limodou@fb567eef-3936-0410-92ba-89b1ab49dacc>
2008-12-04 00:48:13 +0000

    fix typos

--- mixins/mDirBrowser.py
+++ mixins/mDirBrowser.py
@@ -134,7 +134,7 @@ def add_pref(preflist):
     preflist.extend([
 #        (tr('General'), 100, 'num', 'recent_dir_paths_num', tr('Max number of recent browse directories:'), None),
         (tr('General'), 150, 'check', 'open_last_dir_as_startup', tr('Open last directory at startup'), None),
-        (tr('General'), 151, 'check', 'open_project_setting_dlg', tr('Automatically open Project Settings dialog when adding a directory to Directory Browser window'), None),
+        (tr('General'), 151, 'check', 'open_project_setting_dlg', tr('Automatically open the Project Settings dialog if a directory is added into the Directory Browser pane'), None),
         (tr('General'), 160, 'openfile', 'command_line', tr('Command line of Open Command Window Here:'), {'span':True}),
     ])
 Mixin.setPlugin('preference', 'add_pref', add_pref)
